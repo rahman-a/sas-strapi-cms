d4ec8c1b416681945a2e6dc6265367f8fd08a0ae
rahman-a <ahm.abdrahman@gmail.com>
2023-02-11 23:00:02 +0200

    add certification environment variable to database config

--- config/production/database.ts
+++ config/production/database.ts
@@ -1,22 +1,16 @@
 // path: ./config/env/production/database.ts
 
-const parse = require("pg-connection-string").parse;
-
-const { host, port, database, user, password } = parse(
-  process.env.DATABASE_URL
-);
-
 export default ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host,
-      port,
-      database,
-      user,
-      password,
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
       ssl: {
-        ca: env("DATABASE_CA"),
+        ca: env.bool("DATABASE_CA"),
       },
     },
     debug: false,
